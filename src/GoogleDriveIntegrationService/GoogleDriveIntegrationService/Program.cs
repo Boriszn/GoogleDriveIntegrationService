227ee7dba46d0ff6efd53a61f06f4a63c6ef5ef4
boris.zaikin <boris.zaikin@re-flekt.com>
2018-08-29 16:28:06 +0200

    Initial commit

---
+++ src/GoogleDriveIntegrationService/GoogleDriveIntegrationService/Program.cs
@@ -0,0 +1,95 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace GoogleDriveIntegrationService
+{
+    class Program
+    {
+        static string[] Scopes = { DriveService.Scope.Drive };
+        static string ApplicationName = "GoogleDrive Integration Service";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            FilesResource.ListRequest listRequest = service.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";
+
+            // List files.
+            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute()
+                .Files;
+            Console.WriteLine("Files:");
+            if (files != null && files.Count > 0)
+            {
+                foreach (var file in files)
+                {
+                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+            }
+
+            CreateDirectory(service);
+
+            Console.Read();
+
+        }
+
+        public static void CreateDirectory(DriveService _service)
+        {
+            Google.Apis.Drive.v3.Data.File NewDirectory = null;
+
+            // Create metaData for a new Directory
+            Google.Apis.Drive.v3.Data.File body = new Google.Apis.Drive.v3.Data.File();
+            body.Name = "test";
+            body.Description = "description";
+            body.MimeType = "application/vnd.google-apps.folder";
+            // body.Parents = new List<string>() { "te"};
+                //new List() { new ParentReference() { Id = _parent } };
+            try
+            {
+                FilesResource.CreateRequest request = _service.Files.Create(body);
+                NewDirectory = request.Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+        }
+    }
+}
